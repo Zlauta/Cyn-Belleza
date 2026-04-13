bc940017c1d84c05e0f319ec66ebdbb6b900ce31
Zlauta <Zlauuta23@gmail.com>
2026-04-13 08:36:41 -0300

    hotfix: se arreglo imagenes fantasmas y se agrego archivo vercel en back

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,9 +4,10 @@ import app from './app.js';
 // Cargamos las variables de entorno (.env)
 dotenv.config();
 
-const PUERTO = process.env.PORT || 5000;
-
 // Encendemos el servidor
-app.listen(PUERTO, () => {
-  console.log(`🌸 Servidor de CYN Belleza encendido y escuchando en http://localhost:${PUERTO}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  });
+}
